07d88fb394110949d66f33d22c979b10db64be4c
chi.zhang@extremevision.com.cn <chi.zhang@extremevision.com.cn>
2021-09-02 14:43:02 +0800

    modify npm update

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,6 +8,7 @@ interface CokoFlvProps {
     diffSpeedUp?: number;
     latest?: number;
     delay?: number;
+    reconnectLimit?: number;
 }
-declare function CokoFlv({ url, videoEleOpt, mediaDataSource, config, diffCritical, diffSpeedUp, latest, delay, }: CokoFlvProps): JSX.Element;
+declare function CokoFlv({ url, videoEleOpt, mediaDataSource, config, diffCritical, diffSpeedUp, latest, delay, reconnectLimit, }: CokoFlvProps): JSX.Element;
 export default CokoFlv;
